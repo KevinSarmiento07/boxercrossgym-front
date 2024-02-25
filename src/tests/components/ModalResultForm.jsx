a0bb2c808d93b3c65668391211b44d42640c1aef
KevinSarmiento07 <kevinandressasi@ufps.edu.co>
2024-02-24 20:11:32 -0500

    Implementado el eliminar test y creado el component ModalResultForm para digitar el resultado de un test

---
+++ src/tests/components/ModalResultForm.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import Modal from "@mui/material/Modal";
+import { Grid, TextField } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const initialResultForm = {
+  id: 0,
+  fecha: "",
+  valor: "",
+  test: {
+    id: 0,
+  },
+};
+export const ModalResultForm = ({ open, handleClose, idTest, setIdTest }) => {
+  console.log(idTest);
+  useEffect(() => {
+    setResultForm({
+      ...resultForm,
+      test: { id: idTest },
+    });
+  }, [idTest]);
+  const [resultForm, setResultForm] = useState(initialResultForm);
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setResultForm({ ...resultForm, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(resultForm);
+
+    setResultForm(initialResultForm);
+    setIdTest(0);
+  };
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+          <Box sx={style} component="form" onSubmit={onSubmit}>
+            <Typography textAlign="center" variant="h6">
+              Resultado
+            </Typography>
+            <Grid container rowSpacing={2}>
+              <Grid item xs={12}>
+                <TextField variant="outlined" label="Digite su resultado" fullWidth name="valor" value={resultForm.valor} onChange={onInputChange} />
+              </Grid>
+              <Grid item xs={12}>
+                <DatePicker sx={{ width: "100%" }} label="Fecha" name="fecha" value={resultForm.fecha === "" ? null : dayjs(resultForm.fecha)} format="YYYY-MM-DD" required />
+              </Grid>
+              <Grid item xs={12} textAlign={"center"}>
+                <Button variant={"outlined"} type="submit" color="error">
+                  Guardar
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Modal>
+      </LocalizationProvider>
+    </div>
+  );
+};
--- src/tests/components/TestList.jsx
+++ src/tests/components/TestList.jsx
@@ -3,12 +3,20 @@ import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useTests } from "../../hooks/useTests";
 import { useAuth } from "../../hooks/useAuth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ModalResultForm } from "./ModalResultForm";
 
 export const TestList = () => {
-  const { handlerOpenForm, visibleForm, tests, getTests } = useTests();
+  const { handlerOpenForm, visibleForm, tests, getTests, handlerDeleteTest } = useTests();
+  const [idTest, setIdTest] = useState(0);
   console.log(visibleForm);
   const { login } = useAuth();
+  const [open, setOpen] = useState(false);
+  const handleOpen = (id) => {
+    setIdTest(id);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
   console.log(tests);
 
   useEffect(() => {
@@ -26,14 +34,14 @@ export const TestList = () => {
           {tests.length > 0 ? (
             tests.map((test) => {
               return (
-                <Grid item xs={12} sm={6} marginBottom={2} key={test.id} minWidth={200}>
+                <Grid item xs={12} sm={6} md={4} marginBottom={2} key={test.id} minWidth={200}>
                   <Paper elevation={8} sx={{ height: "100%" }}>
                     {!login.isAdmin || (
-                      <IconButton aria-label="delete" sx={{ marginBottom: 0 }} color="error">
+                      <IconButton aria-label="delete" sx={{ marginBottom: 0 }} color="error" onClick={() => handlerDeleteTest(test.id)}>
                         <DeleteIcon />
                       </IconButton>
                     )}
-                    <IconButton aria-label="delete">
+                    <IconButton aria-label="register" onClick={() => handleOpen(test.id)}>
                       <AppRegistrationIcon />
                     </IconButton>
                     <Typography component={"pre"} variant="h6" textAlign={"center"} sx={{ whiteSpace: "pre-wrap" }} fontFamily={"monospace"}>
@@ -50,6 +58,7 @@ export const TestList = () => {
           )}
         </Grid>
       </Box>
+      <ModalResultForm open={open} handleClose={handleClose} idTest={idTest} setIdTest={setIdTest} />
     </>
   );
 };
